740fdf25b8331d3236fafa2da123d6c53a9ebb8f
behrooz <behrooz.zendedel1992@gmail.com>
2022-03-09 10:52:50 +0330

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Discussion from './container/Discussion/Discussion';
 
 function App() {
   return (
     <div className="App">
-     
+      <Discussion />
     </div>
   );
 }
